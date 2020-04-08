71f0c9db5b2c7fba7c49ab6922e1e938cb608793
Anthony <sean.a.protho1@gmail.com>
2020-04-08 12:14:44 -0700

    second

--- duplicate.py
+++ duplicate.py
@@ -1,12 +1,15 @@
-def dup(list):
-	index = 0
-	for n in list:
-		If list[index] == n:
-			return true
-		else:
-			index += 1
-	return false
+def dup(l):
+    index = 0
+    for n in range(len(l) - 1):
+        print("l:" + str(l[index]))
+        print("n:" + str(l[n+1]))
+        if l[index] == l[n+1]:
+            print("True")
+            return True
+        index += 1
+    print("Flase")
+    return False
 
 lists = [[12, 3, 8, 8, 4], [1, 5, 6, 9, 10, 10], [2, 3, 6, 4, 8]]
-for list in lists:
-dup(list)
+for l in lists:
+    dup(l)
